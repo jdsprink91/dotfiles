d6e393d4468e26b387e168e76361bafcf35dc53f
Jason Sprinkle <jason.sprinkle91@gmail.com>
2024-09-26 10:37:16 -0500

    add live grep args to telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,29 @@
 return {
     "nvim-telescope/telescope.nvim",
     branch = '0.1.x',
+    dependencies = {
+        {
+            "nvim-telescope/telescope-live-grep-args.nvim",
+            version = "^1.0.0"
+        }
+    },
     config = function()
         local telescope = require('telescope')
         local builtin = require('telescope.builtin')
+        local lga_actions = require("telescope-live-grep-args.actions")
+        local themes = require("telescope.themes")
+        local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+
+        local layout_config = {
+            width = 0.5
+        }
+
+        local dropdown_theme_no_previewer = themes.get_dropdown({
+            previewer = false,
+            layout_config = layout_config
+        })
+        local dropdown_theme = require('telescope.themes').get_dropdown()
+
         telescope.setup {
             defaults = {
                 file_ignore_patterns = { "node_modules/", ".git/" },
@@ -18,18 +38,28 @@ return {
                 oldfiles = {
                     hidden = true
                 },
+            },
+            extensions = {
+                live_grep_args = {
+                    auto_quoting = true,
+                    mappings = {
+                        i = {
+                            ["<C-f>"] = lga_actions.quote_prompt({ postfix = " -g " })
+                        }
+                    },
+                    theme = dropdown_theme
+                }
             }
         }
 
         -- finding files with no previewer
-        local dropdown_theme_no_previewer = require('telescope.themes').get_dropdown({ previewer = false })
         vim.keymap.set('n', '<leader>ff', function() builtin.find_files(dropdown_theme_no_previewer) end, {})
         vim.keymap.set('n', '<leader>gf', function() builtin.git_files(dropdown_theme_no_previewer) end, {})
 
-        -- these things should get a previewer
-        local dropdown_theme = require('telescope.themes').get_dropdown()
-        vim.keymap.set('n', '<leader>lg', function() builtin.live_grep(dropdown_theme) end, {})
-        vim.keymap.set('n', '<leader>fr', function() builtin.lsp_references(dropdown_theme) end, {})
-        vim.keymap.set('n', '<leader>km', function() builtin.keymaps(dropdown_theme) end, {})
+        -- greps
+        vim.keymap.set('n', '<leader>lg', function()
+            telescope.extensions.live_grep_args.live_grep_args()
+        end, {})
+        vim.keymap.set("n", "<leader>gw", live_grep_args_shortcuts.grep_word_under_cursor)
     end
 }
